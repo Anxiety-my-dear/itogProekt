e0d9118bdaca6703c5f0c7f06d747f062f445429
Aleksey <golovnka14@yandex.ru>
2023-02-06 20:54:54 +0400

    add new tasks

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число N:  ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите число M:  ");
+int m = Convert.ToInt16(Console.ReadLine());
+
+Numbers (n, m);
+
+void Numbers (int n, int m)
+{
+   if (m>n)
+for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+}
---
+++ task68/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int nod = 0;
+
+void NOD(int m, int n)
+{
+ while(m != n)
+            {
+                if(m > n)
+                {
+                    m = m - n;
+                }
+                else
+                {
+                    n = n - m;
+                }
+            }
+
+            nod = n;
+            Console.WriteLine("НОД: " + nod);
+}
+NOD (m,n);
